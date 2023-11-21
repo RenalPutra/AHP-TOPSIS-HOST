725447c1fdb4004208e74b9670efab101edcceee
RenalPro <raenaldsyaputra57@gmail.com>
2023-11-21 23:48:26 +0800

    new

--- spk/settings.py
+++ spk/settings.py
@@ -56,7 +56,7 @@ ROOT_URLCONF = 'spk.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [BASE_DIR, 'templates'],
+        'DIRS': ['templates', os.path.join(BASE_DIR, 'templates')],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
